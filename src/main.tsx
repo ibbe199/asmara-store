b862d6f2038144cebe09b1274bd0040d4151bb57
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-04-03 19:26:12 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
-// قمنا بحذف سطر import './index.css' لحل مشكلة الخطأ في Vercel
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './index.css'  // هذا السطر يجب أن يكون موجوداً
+// باقي الاستيرادات...
